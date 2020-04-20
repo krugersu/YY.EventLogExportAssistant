5fe85329c99f2d385bb477433946969962c63ce5
YPermitin <ypermitin@yandex.ru>
2020-04-21 01:40:15 +0500

    Очистка примеров консольных приложений от неиспользуемого кода

--- Apps/YY.EventLogExportToPostgreSQL/Program.cs
+++ Apps/YY.EventLogExportToPostgreSQL/Program.cs
@@ -12,44 +12,41 @@ namespace YY.EventLogExportToPostgreSQL
     {
         static void Main(string[] args)
         {
-            string connectinString = "";
-            DbContextOptionsBuilder<EventLogContext> optionsBuilder = new DbContextOptionsBuilder<EventLogContext>();
-            optionsBuilder.UseNpgsql(connectinString);
-            
-            using (EventLogContext context = new EventLogContext(optionsBuilder.Options))
+            using (EventLogContext context = new EventLogContext())
             {
                 long isCount = context.InformationSystems.Count();
-                Console.WriteLine(isCount);
+                Console.WriteLine("Количество информационных систем: {0}", isCount);
             }
 
-            //if (args.Length == 0)
-            //{
-            //    Console.WriteLine("Не передан путь к файлам журнала регистрации.");
-            //    return;
-            //}
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Не передан путь к файлам журнала регистрации.");
+            }
+            else
+            {
+                string eventLogPath = args[0];
+                int queueLength = 1000;
+                EventLogTargetDefinition exporterDefination = new EventLogTargetDefinitionForPostgreSQL();
 
-            //string eventLogPath = args[0];
-            //int queueLength = 1000;
-            //EventLogTargetDefinition exporterDefination = new EventLogTargetDefinitionForPostgreSQL();
+                using (EventLogExportMaster exporter = EventLogExportMaster.CreateExportMaster(exporterDefination))
+                {
+                    using (EventLogReader reader = EventLogReader.CreateReader(eventLogPath))
+                    {
+                        if (reader.Read())
+                        {
+                            exporter.AddItem(reader.CurrentRow);
 
-            //using (EventLogExportMaster exporter = EventLogExportMaster.CreateExportMaster(exporterDefination)) 
-            //{
-            //    using (EventLogReader reader = EventLogReader.CreateReader(eventLogPath))
-            //    {
-            //        while (reader.Read())
-            //        {
-            //            exporter.AddItem(reader.CurrentRow);
+                            // Отправляем порцию накопившихся элементов
+                            if (exporter.QueueLength >= queueLength)
+                                exporter.Send();
+                        }
+                    }
 
-            //            // Отправляем порцию накопившихся элементов
-            //            if(exporter.QueueLength >= queueLength)                        
-            //                exporter.Send();                        
-            //        }
-            //    }
-
-            //    // Отправляем оставшиеся элементы
-            //    if (exporter.QueueLength > 0)                
-            //        exporter.Send();                
-            //}
+                    // Отправляем оставшиеся элементы
+                    if (exporter.QueueLength > 0)
+                        exporter.Send();
+                }
+            }
 
             Console.WriteLine("Для выхода нажмите любую клавишу...");
             Console.ReadKey();
--- YY.EventLogExportToSQLServer/Program.cs
+++ YY.EventLogExportToSQLServer/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
+using YY.EventLogExportAssistant;
 using YY.EventLogExportAssistant.SQLServer;
+using YY.EventLogReaderAssistant;
 
 namespace YY.EventLogExportToSQLServer
 {
@@ -9,17 +12,41 @@ namespace YY.EventLogExportToSQLServer
     {
         static void Main(string[] args)
         {
-            string connectinString = "";
-            DbContextOptionsBuilder<EventLogContext> optionsBuilder = new DbContextOptionsBuilder<EventLogContext>();
-            optionsBuilder.UseSqlServer(connectinString);
-
-            using (EventLogContext context = new EventLogContext(optionsBuilder.Options))
+            using (EventLogContext context = new EventLogContext())
             {
                 long isCount = context.InformationSystems.Count();
-                Console.WriteLine(isCount);
+                Console.WriteLine("Количество информационных систем: {0}", isCount);
             }
 
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Не передан путь к файлам журнала регистрации.");
+            }
+            else
+            {
+                string eventLogPath = args[0];
+                int queueLength = 1000;
+                EventLogTargetDefinition exporterDefination = new EventLogTargetDefinitionForSQLServer();
+
+                using (EventLogExportMaster exporter = EventLogExportMaster.CreateExportMaster(exporterDefination))
+                {
+                    using (EventLogReader reader = EventLogReader.CreateReader(eventLogPath))
+                    {
+                        if (reader.Read())
+                        {
+                            exporter.AddItem(reader.CurrentRow);
+
+                            // Отправляем порцию накопившихся элементов
+                            if (exporter.QueueLength >= queueLength)
+                                exporter.Send();
+                        }
+                    }
+
+                    // Отправляем оставшиеся элементы
+                    if (exporter.QueueLength > 0)
+                        exporter.Send();
+                }
+            }
 
             Console.WriteLine("Для выхода нажмите любую клавишу...");
             Console.ReadKey();
