31a221cc8674f7efb8d790245793b5c6edbccfd1
YPermitin <ypermitin@yandex.ru>
2020-05-10 22:56:43 +0500

    Исправил пути в скрипте сборки для Travis CI

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 API_KEY = $1
 
-dotnet nuget push ./YY.EventLogExportAssistant/Libs/YY.EventLogExportAssistant.Core/bin/Release/YY.EventLogExportAssistant.Core.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
-dotnet nuget push ./YY.EventLogExportAssistant/Libs/YY.EventLogExportAssistant.SQLServer\bin\Release/YY.EventLogExportAssistant.SQLServer.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
-dotnet nuget push ./YY.EventLogExportAssistant/Libs/YY.EventLogExportAssistant.PostgreSQL/bin/Release/YY.EventLogExportAssistant.PostgreSQL.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./Libs/YY.EventLogExportAssistant.Core/bin/Release/YY.EventLogExportAssistant.Core.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./Libs/YY.EventLogExportAssistant.SQLServer/bin/Release/YY.EventLogExportAssistant.SQLServer.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ./Libs/YY.EventLogExportAssistant.PostgreSQL/bin/Release/YY.EventLogExportAssistant.PostgreSQL.*.nupkg -k $1 -s https://api.nuget.org/v3/index.json --skip-duplicate
