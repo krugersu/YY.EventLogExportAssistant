1be2e8807d2fac2f655f5ae4c08177ded0d676a6
YPermitin <ypermitin@yandex.ru>
2020-05-17 02:10:40 +0500

    Добавлены тесты для PostgreSQL под AppVeyor

--- Scripts/appveyor-runTests.ps1
+++ Scripts/appveyor-runTests.ps1
@@ -1,2 +1,3 @@
 dotnet test ./Tests/YY.EventLogExportAssistant.Core.Tests/YY.EventLogExportAssistant.Core.Tests.csproj
 dotnet test ./Tests/YY.EventLogExportAssistant.SQLServer.Tests/YY.EventLogExportAssistant.SQLServer.Tests.csproj
+dotnet test ./Tests/YY.EventLogExportAssistant.PostgreSQL.Tests/YY.EventLogExportAssistant.PostgreSQL.Tests.csproj
