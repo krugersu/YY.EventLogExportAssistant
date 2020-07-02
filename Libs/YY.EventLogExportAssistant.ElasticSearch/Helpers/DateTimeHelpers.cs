9910ad7490249d1c1668bb3addec7cfe1ad63cb2
YPermitin <ypermitin@yandex.ru>
2020-07-02 17:12:59 +0500

    Добавил вспомогательные классы для работы с датами и ES

---
+++ Libs/YY.EventLogExportAssistant.ElasticSearch/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YY.EventLogExportAssistant.ElasticSearch.Helpers
+{
+    public static class DateTimeHelpers
+    {
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime StartOfMonth(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, 1);
+        }
+
+        public static DateTime StartOfQuarter(this DateTime dt)
+        {
+            int quarterNumber = (dt.Month - 1) / 3 + 1;
+            DateTime firstDayOfQuarter = new DateTime(dt.Year, (quarterNumber - 1) * 3 + 1, 1);
+            return firstDayOfQuarter;
+        }
+
+        public static DateTime StartOfHalfYear(this DateTime dt)
+        {
+            DateTime start;
+
+            var month = dt.Month;
+            var year = dt.Year;
+            if (month <= 6)
+            {
+                start = new DateTime(year, 1, 1);
+            }
+            else
+            {
+                start = new DateTime(year, 7, 1);
+            }
+
+            return start;
+        }
+
+        public static DateTime StartOfYear(this DateTime dt)
+        {
+            return new DateTime(dt.Year, 1, 1);
+        }
+    }
+}
---
+++ Libs/YY.EventLogExportAssistant.ElasticSearch/Helpers/ElasticSearchHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YY.EventLogExportAssistant.ElasticSearch.Helpers
+{
+    public static class ElasticSearchHelper
+    {
+        public static string GetIndexSeparationPeriod(DateTime period, IndexSeparationPeriod separation)
+        {
+            string separationWithFormat;
+
+            switch (separation)
+            {
+                case IndexSeparationPeriod.Hour:
+                    separationWithFormat = period.ToString("yyyyMMddhh0000");
+                    break;
+                case IndexSeparationPeriod.Day:
+                    separationWithFormat = period.ToString("yyyyMMdd000000");
+                    break;
+                case IndexSeparationPeriod.Week:
+                    separationWithFormat = period.StartOfWeek(DayOfWeek.Monday).ToString("yyyyMMdd000000");
+                    break;
+                case IndexSeparationPeriod.Month:
+                    separationWithFormat = period.StartOfMonth().ToString("yyyyMM01000000");
+                    break;
+                case IndexSeparationPeriod.Quarter:
+                    separationWithFormat = period.StartOfQuarter().ToString("yyyyMM01000000");
+                    break;
+                case IndexSeparationPeriod.HalfYear:
+                    separationWithFormat = period.StartOfHalfYear().ToString("yyyyMM01000000");
+                    break;
+                case IndexSeparationPeriod.Year:
+                    separationWithFormat = period.StartOfYear().ToString("yyyyMM01000000");
+                    break;
+                default:
+                    separationWithFormat = "FULL";
+                    break;
+            }
+
+            return separationWithFormat;
+        }
+    }
+}
