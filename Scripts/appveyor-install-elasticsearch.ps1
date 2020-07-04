049a24baca0b507cddc93efee6f6873be52e1e70
YPermitin <ypermitin@yandex.ru>
2020-07-05 01:27:06 +0500

    Проверка JDK #1

--- Scripts/appveyor-install-elasticsearch.ps1
+++ Scripts/appveyor-install-elasticsearch.ps1
@@ -4,21 +4,7 @@ New-Item "${env:ProgramFiles}\Java" -ItemType Directory -Force | Out-Null
 
 $jdkPath = "${env:ProgramFiles}\Java\jdk11"
 
-if(Test-Path $jdkPath) {
-    Remove-Item $jdkPath -Recurse -Force
-}
-
-Write-Host "Downloading..."
-$zipPath = "$env:TEMP\openjdk-11.0.2_windows-x64_bin.zip"
-(New-Object Net.WebClient).DownloadFile('https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip', $zipPath)
-
-Write-Host "Unpacking..."
-$tempPath = "$env:TEMP\jdk11_temp"
-7z x $zipPath -o"$tempPath" | Out-Null
-[IO.Directory]::Move("$tempPath\jdk-11.0.2", $jdkPath)
-Remove-Item $tempPath -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item $zipPath -ErrorAction SilentlyContinue
-
+cmd dir C:\Program Files\Java\jdk11
 cmd /c "`"$jdkPath\bin\java`" --version"
 
 Write-Host "JDK 11 installed" -ForegroundColor Green
