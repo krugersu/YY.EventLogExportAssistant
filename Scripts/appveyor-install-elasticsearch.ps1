8e4d6189c664b525a16502fc33e8cff3152cc763
YPermitin <ypermitin@yandex.ru>
2020-07-05 01:10:57 +0500

    Добавил в скрипт установку Java (ES)

--- Scripts/appveyor-install-elasticsearch.ps1
+++ Scripts/appveyor-install-elasticsearch.ps1
@@ -1,3 +1,10 @@
+Write-Host "Preparing to download and install Java..." -ForegroundColor Cyan
+$URL=(Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | %{[regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value}
+Invoke-WebRequest -UseBasicParsing -OutFile jre8.exe $URL
+Start-Process .\jre8.exe '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait
+echo $?
+Write-Host "Java installed" -ForegroundColor Green 
+
 Write-Host "Preparing to download and install Elasticsearch..." -ForegroundColor Cyan
 $esVersion = "7.8.0"
 $downloadUrl = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$($esVersion)-windows-x86_64.zip"
