5fdb7e6f8ed0e663b99bb40edfb7e87775804e13
YPermitin <ypermitin@yandex.ru>
2020-07-05 01:16:58 +0500

    Попытка установки JDK 11 для ES

--- Scripts/appveyor-install-elasticsearch.ps1
+++ Scripts/appveyor-install-elasticsearch.ps1
@@ -1,9 +1,27 @@
-Write-Host "Preparing to download and install Java..." -ForegroundColor Cyan
-$URL=(Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | %{[regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value}
-Invoke-WebRequest -UseBasicParsing -OutFile jre8.exe $URL
-Start-Process .\jre8.exe '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait
-echo $?
-Write-Host "Java installed" -ForegroundColor Green 
+Write-Host "Installing JDK 11 ..." -ForegroundColor Cyan
+
+New-Item "${env:ProgramFiles}\Java" -ItemType Directory -Force | Out-Null
+
+$jdkPath = "${env:ProgramFiles}\Java\jdk11"
+
+if(Test-Path $jdkPath) {
+    Remove-Item $jdkPath -Recurse -Force
+}
+
+Write-Host "Downloading..."
+$zipPath = "$env:TEMP\openjdk-11.0.2_windows-x64_bin.zip"
+(New-Object Net.WebClient).DownloadFile('https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip', $zipPath)
+
+Write-Host "Unpacking..."
+$tempPath = "$env:TEMP\jdk11_temp"
+7z x $zipPath -o"$tempPath" | Out-Null
+[IO.Directory]::Move("$tempPath\jdk-11.0.2", $jdkPath)
+Remove-Item $tempPath -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $zipPath -ErrorAction SilentlyContinue
+
+cmd /c "`"$jdkPath\bin\java`" --version"
+
+Write-Host "JDK 11 installed" -ForegroundColor Green
 
 Write-Host "Preparing to download and install Elasticsearch..." -ForegroundColor Cyan
 $esVersion = "7.8.0"
