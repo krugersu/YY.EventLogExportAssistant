b26a0f33eaf16df32ba59510df29c417509c0736
YPermitin <ypermitin@yandex.ru>
2020-07-05 09:28:33 +0500

    Перенес скрипт установки Java в отдельный файл для AppVeyor

---
+++ Scripts/appveyor-install-java.ps1
@@ -0,0 +1,35 @@
+Write-Host "Installing JDK 11 ..." -ForegroundColor Cyan
+
+New-Item "${env:ProgramFiles}\Java" -ItemType Directory -Force | Out-Null
+
+$jdkPath = "${env:ProgramFiles}\Java\jdk11"
+
+if(Test-Path $jdkPath) {
+    Remove-Item $jdkPath -Recurse -Force
+}
+
+Write-Host "Downloading..."
+$zipPath = "$env:TEMP\openjdk-11.0.2_windows-x64_bin.zip"
+(New-Object Net.WebClient).DownloadFile('https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip', $zipPath)
+
+Write-Host "Unpacking..."
+$tempPath = "$env:TEMP\jdk11_temp"
+7z x $zipPath -o"$tempPath" | Out-Null
+[IO.Directory]::Move("$tempPath\jdk-11.0.2", $jdkPath)
+Remove-Item $tempPath -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $zipPath -ErrorAction SilentlyContinue
+
+cmd /c "`"$jdkPath\bin\java`" --version"
+
+Write-Host "Java path:"
+Write-Host $jdkPath
+
+Write-Host "JAVA_HOME (before):"
+Write-Host $env:JAVA_HOME
+
+[Environment]::SetEnvironmentVariable("JAVA_HOME", $jdkPath)
+
+Write-Host "JAVA_HOME (after):"
+Write-Host $env:JAVA_HOME
+
+Write-Host "JDK 11 installed" -ForegroundColor Green
