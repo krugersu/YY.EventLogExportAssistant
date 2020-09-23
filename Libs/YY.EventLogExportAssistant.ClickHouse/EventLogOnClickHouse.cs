60179ce46a2450355017796ad8bbf1194d06f89e
YPermitin <ypermitin@yandex.ru>
2020-09-23 21:27:11 +0500

    Добавлен прототип проекта библиотеки экспорта в ClickHouse

---
+++ Libs/YY.EventLogExportAssistant.ClickHouse/EventLogOnClickHouse.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using YY.EventLogReaderAssistant;
+using RowData = YY.EventLogReaderAssistant.Models.RowData;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace YY.EventLogExportAssistant.ClickHouse
+{
+    public class EventLogOnClickHouse : EventLogOnTarget
+    {
+        #region Private Member Variables
+
+        private const int _defaultPortion = 1000;
+        private readonly int _portion;
+        //private readonly ElasticClient _client;
+        private InformationSystemsBase _system;
+        private string _indexName;
+        //private IndexSeparationPeriod _indexSeparationPeriod;
+        private EventLogPosition _lastEventLogFilePosition;
+
+        #endregion
+
+        #region Constructor
+
+        //public EventLogOnClickHouse() : this(null, _defaultPortion)
+        //{
+
+        //}
+        //public EventLogOnClickHouse(int portion) : this(null, portion)
+        //{
+        //    _portion = portion;
+        //}
+        //public EventLogOnClickHouse(ConnectionSettings elasticSettings, int portion)
+        //{
+        //    _indexSeparationPeriod = IndexSeparationPeriod.None;
+        //    _portion = portion;
+
+        //    if (elasticSettings == null)
+        //    {
+        //        IConfiguration Configuration = new ConfigurationBuilder()
+        //            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        //            .Build();
+        //        IConfigurationSection elasticSearchSection = Configuration.GetSection("ElasticSearch");
+        //        Uri nodeAddress = elasticSearchSection.GetValue<Uri>("Node");
+        //        string indexName = elasticSearchSection.GetValue<string>("IndexName");
+        //        string indexSeparation = elasticSearchSection.GetValue<string>("IndexSeparationPeriod");
+        //        int maximumRetries = elasticSearchSection.GetValue<int>("MaximumRetries");
+        //        int maxRetryTimeout = elasticSearchSection.GetValue<int>("MaxRetryTimeout");
+
+        //        elasticSettings = new ConnectionSettings(nodeAddress)
+        //            .DefaultIndex(indexName)
+        //            .MaximumRetries(maximumRetries)
+        //            .MaxRetryTimeout(TimeSpan.FromSeconds(maxRetryTimeout));
+        //        SetIndexSeparationPeriod(indexSeparation);
+        //    }
+
+        //    _client = new ElasticClient(elasticSettings);
+        //}
+
+        #endregion
+
+        #region Public Methods
+
+        public override EventLogPosition GetLastPosition()
+        {
+            if (_lastEventLogFilePosition != null)
+                return _lastEventLogFilePosition;
+
+            //LogFileElement actualLogFileInfo = _client.GetLastLogFileElement(_system.Name, _indexName);
+            EventLogPosition position = null;
+            //if (actualLogFileInfo != null)
+            //{
+            //    position = new EventLogPosition(
+            //        actualLogFileInfo.LastEventNumber,
+            //        actualLogFileInfo.LastCurrentFileReferences,
+            //        actualLogFileInfo.LastCurrentFileData,
+            //        actualLogFileInfo.LastStreamPosition
+            //    );
+            //}
+            _lastEventLogFilePosition = position;
+            return position;
+        }
+        public override void SaveLogPosition(FileInfo logFileInfo, EventLogPosition position)
+        {
+            SaveLogFileHistoryElement(logFileInfo, position);
+            SaveLogFileActualElement(logFileInfo, position);
+
+            _lastEventLogFilePosition = position;
+        }
+        public override int GetPortionSize()
+        {
+            return _portion;
+        }
+        public override void Save(RowData rowData)
+        {
+            IList<RowData> rowsData = new List<RowData>
+            {
+                rowData
+            };
+            Save(rowsData);
+        }
+        public override void Save(IList<RowData> rowsData)
+        {
+            //Dictionary<string, List<LogDataElement>> logDataByIndices = new Dictionary<string, List<LogDataElement>>();
+
+            //foreach (RowData item in rowsData)
+            //{
+            //    string logDataCurrentIndexName = $"{ _indexName }-LogData-{ item.Period.DateTime.GetIndexSeparationPeriod(_indexSeparationPeriod) }";
+            //    logDataCurrentIndexName = logDataCurrentIndexName.ToLower();
+            //    if (logDataByIndices.ContainsKey(logDataCurrentIndexName) == false)
+            //    {
+            //        logDataByIndices.Add(logDataCurrentIndexName, new List<LogDataElement>());
+            //    }
+
+            //    logDataByIndices[logDataCurrentIndexName].Add(new LogDataElement(_system, item));
+            //}
+
+            //foreach (var indexItems in logDataByIndices)
+            //    _client.SaveLogData(indexItems.Value, indexItems.Key);
+            
+        }
+        public override void SetInformationSystem(InformationSystemsBase system)
+        {
+            //_system = new InformationSystems()
+            //{
+            //    Id = system.Id,
+            //    Name = system.Name,
+            //    Description = system.Description
+            //};
+
+            //if (_indexName == null)
+            //{
+            //    _indexName = _system.Name;
+            //}
+        }
+        public void SetIndexName(string indexName)
+        {
+            _indexName = indexName;
+        }
+        public void SetIndexSeparationPeriod(string separation)
+        {
+            //if (separation != null && Enum.TryParse(separation, true, out IndexSeparationPeriod separationValue))
+            //{
+            //    SetIndexSaparationPeriod(separationValue);
+            //}
+            //else
+            //{
+            //    SetIndexSaparationPeriod(IndexSeparationPeriod.None);
+            //}
+        }
+        //public void SetIndexSaparationPeriod(IndexSeparationPeriod separation)
+        //{
+        //    _indexSeparationPeriod = separation;
+        //}
+        public override void UpdateReferences(ReferencesData data)
+        {
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void SaveLogFileHistoryElement(FileInfo logFileInfo, EventLogPosition position)
+        {
+            //_client.SaveLogFileHistoryElement(new LogFileElement()
+            //{
+            //    Id = $"[{_system.Name}][{logFileInfo.Name}][{logFileInfo.CreationTimeUtc:yyyyMMddhhmmss}]",
+            //    CreateDate = logFileInfo.CreationTimeUtc,
+            //    FileName = logFileInfo.Name,
+            //    InformationSystem = _system.Name,
+            //    LastCurrentFileData = position.CurrentFileData,
+            //    LastCurrentFileReferences = position.CurrentFileReferences,
+            //    LastEventNumber = position.EventNumber,
+            //    LastStreamPosition = position.StreamPosition,
+            //    ModificationDate = logFileInfo.LastWriteTimeUtc
+            //}, _indexName);
+        }
+        private void SaveLogFileActualElement(FileInfo logFileInfo, EventLogPosition position)
+        {
+            //_client.SaveLogFileActualElement(new LogFileElement()
+            //{
+            //    Id = _system.Name,
+            //    CreateDate = logFileInfo.CreationTimeUtc,
+            //    FileName = logFileInfo.Name,
+            //    InformationSystem = _system.Name,
+            //    LastCurrentFileData = position.CurrentFileData,
+            //    LastCurrentFileReferences = position.CurrentFileReferences,
+            //    LastEventNumber = position.EventNumber,
+            //    LastStreamPosition = position.StreamPosition,
+            //    ModificationDate = logFileInfo.LastWriteTimeUtc
+            //}, _indexName);
+        }
+
+        #endregion
+    }
+}
