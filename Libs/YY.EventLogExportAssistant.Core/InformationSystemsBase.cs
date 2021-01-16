fe8573cdc1aadb7e07e3948e7f7fd4eda68e7236
YPermitin <ypermitin@yandex.ru>
2021-01-16 12:30:55 +0500

    Улучшена работа с часовыми поясами

--- Libs/YY.EventLogExportAssistant.Core/InformationSystemsBase.cs
+++ Libs/YY.EventLogExportAssistant.Core/InformationSystemsBase.cs
@@ -1,15 +1,55 @@
 ﻿using System;
+using System.Linq;
 
 namespace YY.EventLogExportAssistant
 {
     public class InformationSystemsBase
     {
+        #region Private Members
+
+        protected string _timeZoneName;
+        protected bool _timeZoneRecognized;
+        protected TimeZoneInfo _timeZone;
+
+        #endregion
+
         #region Public Members
 
         public virtual long Id { get; set; }
         public virtual string Name { get; set; }
         public virtual string Description { get; set; }
-        public virtual string TimeZoneName { get; set; }
+
+        public virtual string TimeZoneName
+        {
+            get => _timeZoneName;
+            set
+            {
+                _timeZoneName = value;
+                if (string.IsNullOrEmpty(_timeZoneName))
+                {
+                    _timeZone = TimeZoneInfo.Local;
+                    _timeZoneName = _timeZone.Id;
+                }
+                else
+                {
+                    _timeZone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(t => t.Id == _timeZoneName);
+                }
+                _timeZoneRecognized = (_timeZone != null);
+            }
+        }
+
+        public virtual TimeZoneInfo TimeZone
+        {
+            get
+            {
+                if (_timeZoneRecognized)
+                {
+                    return _timeZone;
+                }
+
+                throw new TimeZoneNotFoundException();
+            }
+        }
 
         #endregion
 
@@ -17,7 +57,9 @@ namespace YY.EventLogExportAssistant
 
         public InformationSystemsBase()
         {
-            TimeZoneName = TimeZoneInfo.Local.Id;
+            _timeZone = TimeZoneInfo.Local;
+            _timeZoneName = _timeZone.Id;
+            _timeZoneRecognized = true;
         }
 
         #endregion
